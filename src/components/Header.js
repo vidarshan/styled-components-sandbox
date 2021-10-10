f197e31fcc29a871bb5d73e73da2b2e420cf5219
Vidarshan <vidarshanadithya3@gmail.com>
2021-10-10 17:05:59 +0530

    Added styled components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyledHeader } from './styles/Header.styled'
+
+
+const Header = () => {
+    return (
+        <StyledHeader>
+            <h1>Hubble</h1>
+        </StyledHeader>
+    )
+}
+
+export default Header
